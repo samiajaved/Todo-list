58e1172ee83361dca3d652a57004a68ab6d5e1c0
samiajaved <164614308+samiajaved@users.noreply.github.com>
2024-05-06 00:27:00 -0700

    push my todo_list on github.

---
+++ main.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = "true";
+while (condition) {
+    let todayTask = await inquirer.prompt([
+        {
+            name: "todos",
+            type: "input",
+            message: "what do you want to add in your todos?"
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "do you want to add more?",
+            default: "false"
+        }
+    ]);
+    todos.push(todayTask.todos);
+    condition = todayTask.addMore;
+    console.log(todos);
+}
